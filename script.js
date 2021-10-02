68841987a57652a7944b61eb1d7687332c93155c
weldipaula <weldipaula@hotmail.com>
2021-10-01 23:07:52 -0300

    adicionando slides com destaques

---
+++ script.js
@@ -0,0 +1,25 @@
+let time = 4000
+let currentImageIndex = 0
+let images = document.querySelectorAll('#slider .product-container')
+let max = images.length
+
+function nextImage (){
+  
+  images[currentImageIndex].classList.remove('selected')
+
+  currentImageIndex++
+
+  if (currentImageIndex >= max) 
+    currentImageIndex=0
+  
+  images[currentImageIndex].classList.add('selected')
+}
+
+function start () {
+  
+  setInterval(()=> {
+    nextImage()
+  }, time)
+}
+
+window.addEventListener('load', start)
