4a7b03e469b6060c96c50aeaf8a8d2c05f75104d
weldipaula <weldipaula@hotmail.com>
2021-10-05 11:41:57 -0300

    melhorando o script

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let time = 4000
 let currentImageIndex = 0
 let images = document.querySelectorAll('#slider .product-container')
+let dots = document.querySelector('#dots')
 let max = images.length
 
 function nextImage (){
@@ -15,8 +16,19 @@ function nextImage (){
   images[currentImageIndex].classList.add('selected')
 }
 
+function addDots () {
+  for (i = max; i>0; i--) {
+    
+    div = '<div class="dots"></div>'
+
+    dots.innerHTML = div + div
+  }
+ 
+}
+
 function start () {
-  
+
+  addDots()
   setInterval(()=> {
     nextImage()
   }, time)
